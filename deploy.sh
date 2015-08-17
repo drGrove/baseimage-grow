e753ab8f897f1d23c8e99b1a4bcc56d6a5855ca0
Mike Fotinakis <mike@fotinakis.com>
2015-08-17 13:54:44 -0700

    Remove sudo.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sudo docker push grow/baseimage
+docker push grow/baseimage
--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-sudo docker run --rm=true --workdir=/tmp -i grow/baseimage \
+docker run --rm=true --workdir=/tmp -i grow/baseimage \
   bash -c "git clone https://github.com/grow/growsdk.org.git && grow build growsdk.org/"
